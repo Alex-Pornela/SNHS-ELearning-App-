2d0ddde53c97f00b36b6f6825390dde7868c03ad
Alex-Pornela <alexpornela@gmail.com>
2022-04-14 21:36:31 +0800

    Initial Commit

---
+++ app/src/main/java/com/activity/snhs/Grade9/AP/Quarter1.java
@@ -0,0 +1,16 @@
+package com.activity.snhs.Grade9.AP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.activity.snhs.R;
+
+public class Quarter1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_quarter21 );
+    }
+}
